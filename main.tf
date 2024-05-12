fb7434f91db25089c5fac84c520a494cefdae90c
pisa-kun <pisa0401mst@gmail.com>
2024-05-12 13:24:09 +0900

    fix indent

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@
 # location with Standard Storage
 
 resource "google_storage_bucket" "static" {
- name          = "my-bucket-name-is-toge-girl"
- location      = "US"
- storage_class = "STANDARD"
- project    = "<project-id>"
+name          = "my-bucket-name-is-toge-girl"
+location      = "US"
+storage_class = "STANDARD"
+project    = "<project-id>"
 
- uniform_bucket_level_access = true
+uniform_bucket_level_access = true
 }
